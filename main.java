f28d59472531e038f4238939169ae84b30091ade
Sercan Bingöl <100707707+sercanbingol@users.noreply.github.com>
2022-03-15 15:00:15 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+ class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n1, n2, select;
+        System.out.print("Birinci sayıyı giriniz : ");
+        n1 = input.nextInt();
+        System.out.print("İkinci sayıyı giriniz : ");
+        n2 = input.nextInt();
+
+
+        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
+        System.out.print("Seçiminiz : ");
+        select = input.nextInt();
+
+        switch (select) {
+            case 1:
+                System.out.print("Toplam : " + (n1 + n2));
+                break;
+            case 2:
+                System.out.print("Çıkarma : " + (n1 - n2));
+                break;
+            case 3:
+                System.out.print("Çarpma : " + (n1 * n2));
+                break;
+            case 4:
+                if ( n2 == 0) {
+                    System.out.println("Sayı sıfıra bölünemez");
+                } else
+                    System.out.print("Bölme : " + (n1 / n2));
+                break;
+            default:
+                System.out.print("Tekrar deneyiniz ");
+                break;
+        }
+   }
+}
